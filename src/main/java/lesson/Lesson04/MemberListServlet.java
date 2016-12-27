44a58c2d88235a635a81a4a3a3e91aba3da68b00
dhsim86 <dhsim86@gmail.com>
2016-12-27 22:35:06 +0900

    [Lesson04] Adds the library, mysql JDBC driver for java using maven and connected with mysql.

--- src/main/java/lesson/Lesson04/MemberListServlet.java
+++ src/main/java/lesson/Lesson04/MemberListServlet.java
@@ -30,6 +30,52 @@ public class MemberListServlet extends GenericServlet
         Statement stmt = null;
         ResultSet rs = null;
 
+        try
+        {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/studydb",
+                    "study", "study");
+
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery(
+                    "select MNO, MNAME, EMAIL, CRE_DATE" +
+                         " from MEMBERS" +
+                         " order by MNO ASC");
+
+            response.setContentType("text/html; charset=UTF-8");
+
+            PrintWriter out = response.getWriter();
+            out.println("<html><head><title>User list</title></head>");
+            out.println("<body><h1>User list</h1>");
+
+            while (rs.next())
+            {
+                out.println(
+                    rs.getInt("MNO") + ", " +
+                    rs.getString("MNAME") + ", " +
+                    rs.getString("EMAIL") + ", " +
+                    rs.getDate("CRE_DATE") + "<br>"
+                );
+            }
+
+            out.println("</body></html>");
+        }
+        catch (Exception e)
+        {
+            for (StackTraceElement element : e.getStackTrace())
+            {
+                System.out.println(element.toString());
+            }
+            throw new ServletException(e);
+        }
+        finally
+        {
+            try { if (rs != null) rs.close(); } catch (Exception e) { }
+            try { if (stmt != null) stmt.close(); } catch (Exception e) { }
+            try { if (conn != null) conn.close(); } catch (Exception e) { }
+        }
     }
 }
